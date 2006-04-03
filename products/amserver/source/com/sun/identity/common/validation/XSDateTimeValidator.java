d20f200de9a8c1a368958a0fc28de60156645530
veiming <veiming@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2006-04-03 18:37:41 +0000

    Issue number: Obtained from: Submitted by:  Dennis Seah (veiming) Reviewed by:   Shivaram Bhat added common validation classes
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@108 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ products/amserver/source/com/sun/identity/common/validation/XSDateTimeValidator.java
@@ -0,0 +1,78 @@
+/* The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: XSDateTimeValidator.java,v 1.1 2006-04-03 18:37:41 veiming Exp $
+ *
+ * Copyright 2005 Sun Microsystems Inc. All Rights Reserved
+ */
+
+package com.sun.identity.common.validation;
+
+import com.iplanet.am.util.Locale;
+import com.sun.identity.common.DateUtils;
+import java.text.ParseException;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Validator for <code>xs:dateTime</code> format.
+ */
+public class XSDateTimeValidator
+    extends ValidatorBase
+{
+    private static XSDateTimeValidator instance = new XSDateTimeValidator();
+
+    /**
+     * Avoid instantiation of this class.
+     */
+    private XSDateTimeValidator() {
+    }
+
+    public static XSDateTimeValidator getInstance() {
+	return instance;
+    }
+
+    protected void performValidation(String strData)
+	throws ValidationException
+    {
+	if ((strData == null) || (strData.trim().length() == 0)) {
+	    throw new ValidationException(resourceBundleName, "errorCode1"); 
+	}
+
+	if (strData.charAt(strData.length() -1) != 'Z') {
+	    throw new ValidationException(resourceBundleName, "errorCode1"); 
+	}
+
+	try {
+	    DateUtils.stringToDate(strData);
+        } catch (ParseException pe) {
+	    throw new ValidationException(resourceBundleName, "errorCode1"); 
+	}
+    }
+
+    /* Test */
+    public static void main(String[] args) {
+	XSDateTimeValidator inst = getInstance();
+	try {
+	    inst.validate("2009-12-31T12:30:00.0Z");
+	} catch (ValidationException e) {
+	    System.out.println(e.getMessage());
+	}
+    }
+}
