49a6c8610bc50c245b25fc89145985bfcf065ddd
rajeevangal <rajeevangal@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2006-11-06 05:30:58 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@666 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- www/public/use/builds/crttest.js
+++ www/public/use/builds/crttest.js
@@ -449,12 +449,14 @@ alert("SENDING REQ DONE");
 // are replaced with absolute paths.
 function writeResponse( resp)
 {
+alert("WR RESP:"+resp);
     resp = resp.replace('/@import "/g','@import "https://opensso.dev.java.net');
     resp = resp.replace('UTF-8', 'iso-8859-1');
     resp = resp.replace('UTF-8', 'iso-8859-1');
     resp = resp.replace(fixissue1, fixissue2);
     document.write(resp);
     document.close();
+alert("WR RESP close");
 }
 
 // Callback for async ajax requests.
