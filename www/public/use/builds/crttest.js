d4eee2fcc8d88d7ea3b9fbd2db1a4176c99f52fd
rajeevangal <rajeevangal@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2006-11-06 05:38:46 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@668 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- www/public/use/builds/crttest.js
+++ www/public/use/builds/crttest.js
@@ -450,12 +450,12 @@ alert("SENDING REQ DONE");
 function writeResponse( resp)
 {
 alert("WR RESP:"+resp);
-    resp = resp.replace('/@import "/g','@import "https://opensso.dev.java.net');
-    resp = resp.replace('UTF-8', 'iso-8859-1');
-    resp = resp.replace('UTF-8', 'iso-8859-1');
-    resp = resp.replace(fixissue1, fixissue2);
-    //document.write(resp);
-    document.write("<html><body> HELLO </body></html>");
+    //resp = resp.replace('/@import "/g','@import "https://opensso.dev.java.net');
+    //resp = resp.replace('UTF-8', 'iso-8859-1');
+    //resp = resp.replace('UTF-8', 'iso-8859-1');
+    //resp = resp.replace(fixissue1, fixissue2);
+    document.write(resp);
+    //document.write("<html><body> HELLO </body></html>");
     document.close();
 alert("WR RESP close");
 }
