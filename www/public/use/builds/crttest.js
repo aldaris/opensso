cdc8746bda1f75dd3814f7744b46d44ba3ca6e61
rajeevangal <rajeevangal@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2006-11-06 07:47:48 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@671 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- www/public/use/builds/crttest.js
+++ www/public/use/builds/crttest.js
@@ -453,7 +453,7 @@ function writeResponse( resp)
 alert("WR RESP:"+resp);
     resp = resp.replace('UTF-8', 'iso-8859-1');
     resp = resp.replace('UTF-8', 'iso-8859-1');
-    resp = resp.replace(fixissue1, fixissue2);
+    //resp = resp.replace(fixissue1, fixissue2);
     document.write(resp);
     //document.write("<html><body> HELLO </body></html>");
     document.close();
