85c5f99700b2e8378b8bb33584ba36ccd1c5da2a
bt199000 <bt199000@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2007-02-23 23:46:30 +0000

    Issue 317: New amtune scripts for Window platform
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@970 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ products/amserver/scripts/tune/windows/amtune.pl
@@ -0,0 +1,114 @@
+#
+# The contents of this file are subject to the terms
+# of the Common Development and Distribution License
+# (the License). You may not use this file except in
+# compliance with the License.
+#
+# You can obtain a copy of the License at
+# https://opensso.dev.java.net/public/CDDLv1.0.html or
+# opensso/legal/CDDLv1.0.txt
+# See the License for the specific language governing
+# permission and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL
+# Header Notice in each file and include the License file
+# at opensso/legal/CDDLv1.0.txt.
+# If applicable, add the following below the CDDL Header,
+# with the fields enclosed by brackets [] replaced by
+# your own identifying information:
+# "Portions Copyrighted [year] [name of copyright owner]"
+#
+# $Id: amtune.pl,v 1.1 2007-02-23 23:46:30 bt199000 Exp $
+#
+# Copyright 2007 Sun Microsystems Inc. All Rights Reserved
+#
+#############################################################################
+# Start of main program
+#############################################################################
+use File::Basename;
+
+require "amtune-utils.pl";
+
+my $SCRIPT_LOCATION=basename($0);
+my $AMTUNE_SCRIPT_RECORD_STRING="AMTUNE_MAIN_SCRIPT";
+my $AMTUNE_BIN_DIR;
+
+$PERL_CMD="perl";
+
+
+#import the environment
+if(-f "$SCRIPT_LOCATION\amtune-env.pl")
+{
+	if($INIT_STATUS ne INIT_COMPLETE)
+	{
+		my $SCRIPT="$SCRIPT_LOCATION\amtune-env.pl";
+		@args=($PERL_CMD,$SCRIPT);
+		system(@args)==0 or die "\n Error executing command @args\n";
+		#`perl "$SCRIPT_LOCATION\amtune-env.pl";
+	}
+}
+
+print "Access Manager Tuning Script\n";
+print "$LINE_SEP"."\n";;
+
+
+#Tune Web Server/Application Server
+@webappserver=grep(m/$AMTUNE_TUNE_WEB_CONTAINER=true/,@filecontent);
+if($#webappserver>-1)
+{
+	&webContainerToTune;
+	@wc_config=grep(m/$WC_CONFIG=\s*/,@filecontent);
+	if(@wc_config>-1)
+	{	
+		if(-f $AMTUNE_BIN_DIR\$WC_CONFIG)
+		{	
+			my $SCRIPT="$AMTUNE_BIN_DIR/$WC_CONFIG";
+			@args=($PERL_CMD,$SCRIPT);
+			system(@args)==0 or die "\n Error executing command @args\n";
+			#`perl $AMTUNE_BIN_DIR/$WC_CONFIG`;
+			if($? != 0)
+			{	
+				print "WARNING:$WC_CONFIG is not running. Script is not found in $AMTUNE_BIN_DIR\n";
+			}
+		}
+	}
+}
+
+
+#Tune Directory Server
+@amtune_tune_ds=grep(m/AMTUNE_TUNE_DS=true/,@filecontent);
+if($#amtune_tune_ds>-1)
+{	
+	if(-f "$AMTUNE_BIN_DIR\amtune-prepareDSTuner.pl")
+	{
+		my $SCRIPT="$AMTUNE_BIN_DIR\amtune-prepareDSTuner.pl";
+		@args=($PERL_CMD,$SCRIPT);
+		system(@args)==0 or die "\n Error executing command @args\n";
+		#`perl $AMTUNE_BIN_DIR\amtune-prepareDSTuner.pl`;
+		if($?!=0)
+		{
+			print "WARNING:amtune-prepareDSTuner.pl is not running. Script is not found in $AMTUNE_BIN_DIR\n";
+		}
+	}
+}
+
+
+#Tune Access Manager
+@amtune_tune_identity=grep(m/$AMTUNE_TUNE_IDENTITY=true/,@filecontent);
+if($#amtune_tune_identity>-1)
+{
+	if(-f "$AMTUNE_BIN_DIR\amtune-identity.pl")
+	{
+		my $SCRIPT="$AMTUNE_BIN_IDENTITY\amtune-identity.pl";
+		@args=($PERL_CMD,$SCRIPT);
+		system(@args)==0 or die "\n Error executing command @args\n";
+		#`perl $AMTUNE_BIN_IDENTITY\amtune-identity.pl`;
+		if($?!=0)
+		{
+			print "WARNING:amtune-identity.pl is not running. Script is not found in $AMTUNE_BIN_DIR\n";
+		}
+	}
+}
+
+print "Tuning Complete\n":
+print "$CHAPTER_SEP\n";
