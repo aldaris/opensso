b583c8e26d8d02dc4e6c9257b1c5585a0c7ceeff
mrudulahg <mrudulahg@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2007-03-29 21:41:37 +0000

    Integrated webtest given by Dennis
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@1073 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ qatest/source/com/sun/identity/qatest/common/webtest/DataEntry.java
@@ -0,0 +1,162 @@
+/* The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: DataEntry.java,v 1.1 2007-03-29 21:41:36 mrudulahg Exp $
+ *
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ */
+
+package com.sun.identity.qatest.common.webtest;
+
+import com.gargoylesoftware.htmlunit.Page;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataEntry {
+    private String visitURLPattern;
+    private URL url;
+    private String formName;
+    private String btnName;
+    private String expectedResult;
+    private Map<String, String> inputs = new HashMap<String, String>();
+    private Map<String, String> textareas = new HashMap<String, String>();
+    private Map<String, String> checkboxes = new HashMap<String, String>();
+    private Map<String, String[]> dynamicInputs =
+        new HashMap<String, String[]>();
+    
+    DataEntry() {
+    }
+
+    DataEntry(URL url) {
+        this.url = url;
+    }
+    
+    DataEntry(String url) {
+        visitURLPattern = url;
+    }
+    
+    DataEntry(
+        String formName,
+        String btnName,
+        Map<String, String> inputs,
+        Map<String, String> textareas,
+        Map<String, String> checkboxes,
+        Map<String, String[]> dynamicInputs,
+        String expectedResult
+    ) {
+        this.formName = formName;
+        this.btnName = btnName;
+        this.inputs.putAll(inputs);
+        this.textareas.putAll(textareas);
+        this.checkboxes.putAll(checkboxes);
+        this.dynamicInputs.putAll(dynamicInputs);
+        this.expectedResult = expectedResult;
+    }
+
+    /**
+     * Returns form name.
+     *
+     * @return form name.
+     */
+    public String getFormName() {
+        return formName;
+    }
+    
+    /**
+     * Returns the URL to visit.
+     *
+     * @return the URL to visit.
+     */
+    public String getVisitURLPattern() {
+        return visitURLPattern;
+    }
+
+    /**
+     * Returns the URL to visit.
+     *
+     * @return the URL to visit.
+     */
+    public URL getVisitURL() {
+        return url;
+    }
+    /**
+     * Returns submit button name.
+     *
+     * @return submit button name.
+     */
+    public String getButtonName() {
+        return btnName;
+    }
+
+    /**
+     * Sets expected result.
+     *
+     * @param result Expected result.
+     */
+    public void setExpectedResult(String result) {
+        expectedResult = result;
+    }
+
+    /**
+     * Validate if the web response is correct.
+     */
+    public void validate(Page page) {
+        if (expectedResult != null) {
+            String content = page.getWebResponse().getContentAsString();
+            assert(content.indexOf(expectedResult) != -1);
+        }
+    }
+ 
+    /**
+     * Returns form checkbox values.
+     *
+     * @return form checkbox values.
+     */
+    public Map<String, String> getFormCheckboxValues() {
+        return checkboxes;
+    }
+
+    /**
+     * Returns form input values.
+     *
+     * @return form input values.
+     */
+    public Map<String, String> getFormInputValues() {
+        return inputs;
+    }
+    
+    /**
+     * Returns form textarea values.
+     *
+     * @return form textarea values.
+     */
+    public Map<String, String> getFormTextAreaValues() {
+        return textareas;
+    }
+
+    /**
+     * Returns form dynamic input values.
+     *
+     * @return form dynamic input values.
+     */
+    public Map<String, String[]> getFormDynamicInputValues() {
+        return dynamicInputs;
+    }
+}
---
+++ qatest/source/com/sun/identity/qatest/common/webtest/DefaultTaskHandler.java
@@ -0,0 +1,66 @@
+/* The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: DefaultTaskHandler.java,v 1.1 2007-03-29 21:41:36 mrudulahg Exp $
+ *
+ * Copyright 2007 Sun Microsystems Inc. All Rights Reserved
+ */
+
+package com.sun.identity.qatest.common.webtest;
+
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.sun.identity.qatest.common.webtest.DataEntry;
+import com.sun.identity.qatest.common.webtest.DataSet;
+import com.sun.identity.qatest.common.webtest.WebUtils;
+import com.sun.identity.qatest.common.webtest.ITask;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * This class is responsible for configuring the product.
+ */
+public class DefaultTaskHandler implements ITask {
+    private String base;
+    
+    public DefaultTaskHandler(String base) {
+        this.base = base.toLowerCase();
+    }
+    
+    /**
+     * Executes the task i.e. visiting the URL and post information.
+     *
+     * @param webClient Web Client to hit the URL.
+     * @return the resulting HTML page.
+     * @throws Exception if task cannot be executed or assertion failed.
+     */
+    public HtmlPage execute(WebClient webClient)
+        throws Exception {
+        DataSet data = new DataSet(base);
+        URL url = new URL(data.getKickOffURL());
+        System.out.println(url);
+        HtmlPage page = (HtmlPage)webClient.getPage(url);
+        List<DataEntry> entries = data.getEntries();
+        for (DataEntry e : entries) {
+            page = (HtmlPage)WebUtils.postForm(webClient, page, e);
+        }
+        return page;
+    }
+}
