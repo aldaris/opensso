c413b8c4d368c33f9541e84435eb208f5d8e54d0
hengming <hengming@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2007-10-04 04:52:14 +0000

    Issue number:  1036 Obtained from: Submitted by:  Heng-Ming Hsu Reviewed by:   Qingwen Cheng
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@1933 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ products/federation/library/source/com/sun/identity/saml2/servlet/IDPSingleSignOnServiceSOAP.java
@@ -0,0 +1,60 @@
+/* The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: IDPSingleSignOnServiceSOAP.java,v 1.1 2007-10-04 04:52:14 hengming Exp $
+ *
+ * Copyright 2007 Sun Microsystems Inc. All Rights Reserved
+ */
+
+
+package com.sun.identity.saml2.servlet;
+
+import com.sun.identity.saml.common.SAMLUtils;
+import com.sun.identity.saml2.profile.IDPSSOFederate;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+
+/**
+ * This class <code>SPSingleSignOnServiceSOAP</code> receives and processes 
+ * AuthnRequest using SOAP binding on IDP side.
+ */
+public class IDPSingleSignOnServiceSOAP extends HttpServlet {
+
+    public void init() throws ServletException {
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+            
+        // handle DOS attack
+        SAMLUtils.checkHTTPContentLength(req);
+        IDPSSOFederate.doSSOFederate(req, resp, true);
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+            
+        // handle DOS attack
+        SAMLUtils.checkHTTPContentLength(req);
+        IDPSSOFederate.doSSOFederate(req, resp, true);
+    }
+}
---
+++ products/federation/library/source/com/sun/identity/saml2/servlet/SPECPService.java
@@ -0,0 +1,70 @@
+/* The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: SPECPService.java,v 1.1 2007-10-04 04:52:14 hengming Exp $
+ *
+ * Copyright 2007 Sun Microsystems Inc. All Rights Reserved
+ */
+
+package com.sun.identity.saml2.servlet;
+
+import com.sun.identity.saml.common.SAMLUtils;
+import com.sun.identity.saml2.common.SAML2Exception;
+import com.sun.identity.saml2.common.SAML2Utils;
+import com.sun.identity.saml2.profile.SPSSOFederate;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+
+
+/**
+ * This class <code>SPSingleLogoutServiceSOAP</code> receives and processes 
+ * single logout request using SOAP binding on SP side.
+ */
+public class SPECPService extends HttpServlet {
+
+    public void init() throws ServletException {
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        doGetPost(req, resp);
+    }
+            
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        doGetPost(req, resp);
+    }
+
+    private void doGetPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        try {
+            // handle DOS attack
+            SAMLUtils.checkHTTPContentLength(req);
+            SPSSOFederate.initiateECPRequest(req, resp);
+
+        } catch (SAML2Exception ex) {
+            SAML2Utils.debug.error("SPECPService", ex);
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                 ex.getMessage());
+        }
+    }
+}
