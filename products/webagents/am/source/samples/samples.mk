c5e64143a3ae512b37fbffc0e2665887aaedf430
madan_ranganath <madan_ranganath@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2007-10-12 21:49:27 +0000

    Common installer changes, fix for issue # 1090
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@1981 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- products/webagents/am/source/samples/samples.mk
+++ products/webagents/am/source/samples/samples.mk
@@ -1,37 +1,38 @@
-/* The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License). You may not use this file except in
- * compliance with the License.
- *
- * You can obtain a copy of the License at
- * https://opensso.dev.java.net/public/CDDLv1.0.html or
- * opensso/legal/CDDLv1.0.txt
- * See the License for the specific language governing
- * permission and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at opensso/legal/CDDLv1.0.txt.
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
- *
- * Makefile for building sample agent
- */ 
+#
+# The contents of this file are subject to the terms
+# of the Common Development and Distribution License
+# (the License). You may not use this file except in
+# compliance with the License.
+#
+# You can obtain a copy of the License at
+# https://opensso.dev.java.net/public/CDDLv1.0.html or
+# opensso/legal/CDDLv1.0.txt
+# See the License for the specific language governing
+# permission and limitations under the License.
+# 
+# When distributing Covered Code, include this CDDL
+# Header Notice in each file and include the License file
+# at opensso/legal/CDDLv1.0.txt.
+# If applicable, add the following below the CDDL Header,
+# with the fields enclosed by brackets [] replaced by
+# your own identifying information:
+# "Portions Copyrighted [year] [name of copyright owner]"
+#
+# Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+#
+# Makefile for building sample agent
+#
 
 OS_ARCH := $(shell uname -s)
 
 ifndef AM_INCLUDE_DIR
-	AM_INCLUDE_DIR := ../../include
+	AM_INCLUDE_DIR := ../include
 endif
 ifndef AM_LIB_DIR
 ifeq ($(OS_ARCH), WINNT)
-	AM_LIB_DIR := ../../bin
+	AM_LIB_DIR := ../bin
 else
-	AM_LIB_DIR := ../../lib
+	AM_LIB_DIR := ../lib
 endif
 endif
 
