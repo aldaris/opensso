f3627563188fb429356955f8b8b3139b05bebb9e
rmisra <rmisra@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2007-11-07 21:55:35 +0000

    Issue number: 1178 Obtained from: /home/rm73767/qatest_work/opensso/qatest Submitted by: rmisra Reviewed by:  mrudulahg
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@2146 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ qatest/source/com/sun/identity/qatest/policy/ConfigUnconfig.java
@@ -0,0 +1,66 @@
+/* The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: ConfigUnconfig.java,v 1.1 2007-11-07 21:55:35 rmisra Exp $
+ *
+ * Copyright 2007 Sun Microsystems Inc. All Rights Reserved
+ */
+
+package com.sun.identity.qatest.policy;
+
+import com.sun.identity.qatest.common.TestCommon;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * This class starts and stops the notification server.
+ */
+public class ConfigUnconfig extends TestCommon {
+    
+    /**
+     * Creates a new instance of ConfigUnconfig
+     */
+    public ConfigUnconfig() {
+        super("ConfigUnconfig");
+    }
+    
+    /**
+     * Start the notification (jetty) server for getting notifications from the
+     * server.
+     */
+    @BeforeSuite(groups={"ds_ds", "ds_ds_sec", "ff_ds", "ff_ds_sec"})
+    public void startServer()
+    throws Exception {
+        entering("startServer", null);
+        startNotificationServer();
+        exiting("startServer");
+    }
+
+    /**
+     * Stop the notification (jetty) server for getting notifications from the
+     * server.
+     */
+    @AfterSuite(groups={"ds_ds", "ds_ds_sec", "ff_ds", "ff_ds_sec"})
+    public void stopServer()
+    throws Exception {
+        entering("stopServer", null);
+        stopNotificationServer();
+        exiting("stopServer");
+    }
+}
