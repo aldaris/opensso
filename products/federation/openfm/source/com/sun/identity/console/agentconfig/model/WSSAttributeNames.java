f77eea27e0d8eb9ceced04d0e0213b83ac12222b
veiming <veiming@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2007-12-17 20:17:35 +0000

    Issue: 1373 Submitted by: dennis reviewed by: jon agent configuration UI
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@2427 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ products/federation/openfm/source/com/sun/identity/console/agentconfig/model/WSSAttributeNames.java
@@ -0,0 +1,46 @@
+/* The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: WSSAttributeNames.java,v 1.1 2007-12-17 20:17:35 veiming Exp $
+ *
+ * Copyright 2007 Sun Microsystems Inc. All Rights Reserved
+ */
+
+package com.sun.identity.console.agentconfig.model;
+
+/**
+ * Attribute names of WSS Service.
+ */
+public interface WSSAttributeNames {
+    String SECURITY_MECH = "SecurityMech";
+    String USE_DEFAULT_KEYSTORE = "useDefaultStore";
+    String KEY_STORE_LOCATION = "KeyStoreFile";
+    String KEY_STORE_PASSWORD = "KeyStorePassword";
+    String KEY_PASSWORD = "KeyPassword";
+    String CERT_ALIAS = "privateKeyAlias";
+    String USERCREDENTIAL = "UserCredential";
+    String USERCREDENTIAL_NAME = "UserName:";
+    String USERCREDENTIAL_PWD = "UserPassword:";
+    String STS_ENDPOINT = "STSEndpoint";
+    String STS_MEX_ENDPOINT = "STSMexEndpoint";
+    String AUTH_CHAIN = "authenticationChain";
+    String PASSWORD = "userpassword";
+    String STS = "STS";
+    String DISCOVERY = "Discovery";
+}
