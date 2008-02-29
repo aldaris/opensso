f0d3c246e9bcbe675846085bc4eac5bc6c528959
mahesh_prasad_r <mahesh_prasad_r@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2008-02-29 12:27:43 +0000

    Issue number: 1755 Obtained from: Submitted by:  Mahesh Prasad Reviewed by:   Dennis Seah Log Message: Updating translated OLH 70% Reviewed files in 7 locales
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@2921 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ products/amserver/docs/console/javahelp/ko_KR/app.hs
@@ -0,0 +1,29 @@
+<?xml version='1.0' encoding='euc-kr'?>
+<!-- ident   "%Z%%M% %I%   %E% SMI" -->
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "helpset_1_0.dtd">
+
+<helpset version="2.0" xml:lang="ko">
+<title>Sun Java System Federated Access Manager 8.0 콘솔 온라인 도움말</title>
+<maps>
+	<homeID></homeID>
+	<mapref location="map.jhm"/>
+</maps>
+<view>
+	<name>TOC</name>
+	<label>목차</label>
+	<type>javax.help.TOCView</type>
+	<data>toc.xml</data>
+</view>
+<view>
+	<name>Index</name>
+	<label>색인</label>
+	<type>javax.help.IndexView</type>
+	<data>index.xml</data>
+</view>
+<view>
+	<name>Search</name>
+	<label>검색</label>
+	<type>javax.help.SearchView</type>
+	<data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+</view>
+</helpset>
