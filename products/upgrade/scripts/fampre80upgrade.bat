c4eb72818968cad608c6537eea298041da2c6321
bina <bina@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2008-05-21 23:42:59 +0000

    Issue 1559 - adding preupgrade, classpath add in famupgrade.bat
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@3521 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ products/upgrade/scripts/fampre80upgrade.bat
@@ -0,0 +1,50 @@
+@echo off
+
+: The contents of this file are subject to the terms
+: of the Common Development and Distribution License
+: (the License). You may not use this file except in
+: compliance with the License.
+:
+: You can obtain a copy of the License at
+: https://opensso.dev.java.net/public/CDDLv1.0.html or
+: opensso/legal/CDDLv1.0.txt
+: See the License for the specific language governing
+: permission and limitations under the License.
+:
+: When distributing Covered Code, include this CDDL
+: Header Notice in each file and include the License file
+: at opensso/legal/CDDLv1.0.txt.
+: If applicable, add the following below the CDDL Header,
+: with the fields enclosed by brackets [] replaced by
+: your own identifying information:
+: "Portions Copyrighted [year] [name of copyright owner]"
+:
+: $Id: fampre80upgrade.bat,v 1.1 2008-05-21 23:42:59 bina Exp $
+:
+: Copyright 2008 Sun Microsystems Inc. All Rights Reserved
+
+@echo off
+setlocal
+:WHILE
+if x%1==x goto WEND
+set PARAMS=%PARAMS% %1
+shift
+goto WHILE
+:WEND
+
+echo ===================================================
+echo FAM 8.0 Pre-Upgrade
+echo ===================================================
+echo.
+
+set /p UPGRADE_DIR=Enter the FAM 8.0 base directory :
+echo.
+set /p STAGING_DIR=Enter the FAM 8.0 staging directory :
+echo.
+set /p CONFIG_DIR=Enter the FAM configuration directory :
+
+"%JAVA_HOME%/bin/java.exe" -Xms64m -Xmx256m -cp "%CONFIG_DIR%;%STAGING_DIR%\WEB-INF\lib\ldapjdk.jar;%STAGING_DIR%\WEB-INF\lib\opensso-sharedlib.jar;%UPGRADE_DIR%\upgrade\lib\upgrade.jar;%STAGING_DIR%\WEB-INF\lib\opensso.jar" -D"basedir=%UPGRADE_DIR%" -D"stagingDir=%STAGING_DIR%" -D"configDir=%CONFIG_DIR%" com.sun.identity.upgrade.FAMPreUpgrade %PARAMS% 
+endlocal
+:END
+echo.
+echo ===================================================
--- products/upgrade/scripts/famupgrade.bat
+++ products/upgrade/scripts/famupgrade.bat
@@ -19,9 +19,9 @@
 : your own identifying information:
 : "Portions Copyrighted [year] [name of copyright owner]"
 :
-: $Id: famupgrade.bat,v 1.1 2008-01-18 08:03:12 bina Exp $
+: $Id: famupgrade.bat,v 1.2 2008-05-21 23:42:59 bina Exp $
 :
-: Copyright 2007 Sun Microsystems Inc. All Rights Reserved
+: Copyright 2008 Sun Microsystems Inc. All Rights Reserved
 
 set TOOLS_HOME="STAGING_DIR"
 set CONFIG_DIR="FAM_CONFIG_DIR"
@@ -35,6 +35,6 @@ shift
 goto WHILE
 :WEND
 
-"%JAVA_HOME%/bin/java.exe" -Xms64m -Xmx256m -cp  %UPGRADE_DIR%/lib/amadm_setup.jar;%UPGRADE_DIR%/lib/upgrade.jar;%UPGRDE_DIR%/lib/legacy.jar;%CONFIG_DIR%;%TOOLS_HOME%/locale;%TOOLS_HOME%/lib/amadm_setup.jar;%TOOLS_HOME%/lib/OpenDS.jar;%TOOLS_HOME%/classes;%TOOLS_HOME%/lib/ldapjdk.jar;%TOOLS_HOME%/lib/mail.jar;%TOOLS_HOME%/lib/j2ee.jar;%TOOLS_HOME%/lib/webservices-api.jar;%TOOLS_HOME%/lib/webservices-rt.jar;%TOOLS_HOME%/lib/webservices-tools.jar;%TOOLS_HOME%/lib/xsdlib.jar;%TOOLS_HOME%/lib/xmlsec.jar;%TOOLS_HOME%/lib/opensso.jar;%TOOLS_HOME%/lib/opensso-sharedlib.jar;%TOOLS_HOME%/lib/fam.jar;%TOOLS_HOME%/lib/openfedlib.jar -D"com.iplanet.am.serverMode=true" -D"bootstrap.dir=@CONFIG_DIR@" -D"definitionFiles=com.sun.identity.cli.AccessManager,com.sun.identity.federation.cli.FederationManager" -D"amconfig=AMConfig" -D"java.util.logging.manager=com.sun.identity.log.LogManager" -D"java.util.logging.config.class=com.sun.identity.log.s1is.LogConfigReader" -D"java.version.current=java.vm.version" -D"java.version.expected=1.4+" -D"am.version.current=com.iplanet.am.version" -D"am.version.expected=8.0" com.sun.identity.upgrade.FAMUpgrade %PARAMS%
+"%JAVA_HOME%/bin/java.exe" -Xms64m -Xmx256m -cp  "%UPGRADE_DIR%/locale;%UPGRADE_DIR%/lib/amadm_setup.jar;%UPGRADE_DIR%/lib/upgrade.jar;%UPGRADE_DIR%/lib/legacy.jar;%CONFIG_DIR%;%TOOLS_HOME%/locale;%TOOLS_HOME%/lib/amadm_setup.jar;%TOOLS_HOME%/lib/OpenDS.jar;%TOOLS_HOME%/classes;%TOOLS_HOME%/lib/ldapjdk.jar;%TOOLS_HOME%/lib/mail.jar;%TOOLS_HOME%/lib/j2ee.jar;%TOOLS_HOME%/lib/webservices-api.jar;%TOOLS_HOME%/lib/webservices-rt.jar;%TOOLS_HOME%/lib/webservices-tools.jar;%TOOLS_HOME%/lib/xsdlib.jar;%TOOLS_HOME%/lib/xmlsec.jar;%TOOLS_HOME%/lib/opensso.jar;%TOOLS_HOME%/lib/opensso-sharedlib.jar;%TOOLS_HOME%/lib/fam.jar;%TOOLS_HOME%/lib/openfedlib.jar" -D"com.iplanet.am.serverMode=true" -D"bootstrap.dir=%CONFIG_DIR%" -D"definitionFiles=com.sun.identity.cli.AccessManager,com.sun.identity.federation.cli.FederationManager" -D"amconfig=AMConfig" -D"java.util.logging.manager=com.sun.identity.log.LogManager" -D"java.util.logging.config.class=com.sun.identity.log.s1is.LogConfigReader" -D"java.version.current=java.vm.version" -D"java.version.expected=1.4+" -D"am.version.current=com.iplanet.am.version" -D"am.version.expected=8.0" -D"configDir=%CONFIG_DIR%" com.sun.identity.upgrade.FAMUpgrade %PARAMS%
 endlocal
 :END
