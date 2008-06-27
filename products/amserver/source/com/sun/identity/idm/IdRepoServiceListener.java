3efc22b7a2c7caef1464006c94d5ea645d5bc577
arviranga <arviranga@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2008-06-27 22:36:33 +0000

    Issues 2859: IdRepo service should not initializes DAI (i.e., AMSDK) service if not enabled
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@3908 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- products/amserver/source/com/sun/identity/idm/IdRepoServiceListener.java
+++ products/amserver/source/com/sun/identity/idm/IdRepoServiceListener.java
@@ -22,85 +22,16 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * $Id: IdRepoServiceListener.java,v 1.7 2008-06-25 05:43:29 qcheng Exp $
+ * $Id: IdRepoServiceListener.java,v 1.8 2008-06-27 22:36:33 arviranga Exp $
  *
  */
 
 package com.sun.identity.idm;
 
-import com.sun.identity.shared.debug.Debug;
-import com.sun.identity.sm.ServiceListener;
+/**
+ * Class implementation moved to com.sun.identity.server.IdRepoPluginsCache
+ */
 
-public class IdRepoServiceListener implements ServiceListener {
+public class IdRepoServiceListener {
 
-    Debug debug = AMIdentityRepository.debug;
-
-    public IdRepoServiceListener() {
-        // do nothing
-    }
-
-    /**
-     * Notification for global config changes to IdRepoService
-     */
-    public void globalConfigChanged(String serviceName, String version,
-            String groupName, String serviceComponent, int type) {
-        IdServices idServices = IdServicesFactory.getDataStoreServices();
-        if (debug.messageEnabled()) {
-            debug.message("IdRepoServiceListener: Global Config changed"
-                    + " called");
-        }
-        if (serviceComponent.equals("") || serviceComponent.equals("/")) {
-            return;
-        }
-        // FIXME: Clients don't have to call this !!
-        if (!serviceComponent.startsWith("/users/") && 
-            !serviceComponent.startsWith("/roles/")
-        ) {
-            if (type != 1) {
-                idServices.clearIdRepoPlugins();
-            }
-        }
-
-        // Clear IdUtils.getOrganization(...) cache
-        IdUtils.clearOrganizationNamesCache();
-    }
-
-    /**
-     * Notification for organization config changes to IdRepoService
-     */
-    public void organizationConfigChanged(String serviceName, String version,
-            String orgName, String groupName, String serviceComponent, int type)
-    {
-        IdServices idServices = IdServicesFactory.getDataStoreServices();
-        if (debug.messageEnabled()) {
-            debug.message("IdRepoServiceListener: Org Config changed called");
-        }
-        // ignore componenet is "". 
-        // if component is /" and type is 2(delete), need to remove hidden
-        // plugins.
-        if ((type == ServiceListener.REMOVED) &&
-                serviceComponent.equals("/")) {
-            idServices.clearIdRepoPlugins(orgName, serviceComponent, type);
-        } else if (!serviceComponent.equals("") && 
-                   !serviceComponent.equals("/")) {
-            idServices.clearIdRepoPlugins(orgName, serviceComponent, type);
-        }
-
-        // Clear IdUtils.getOrganization(...) cache
-        IdUtils.clearOrganizationNamesCache();
-    }
-
-    /**
-     * Notification for schema changes to IdRepoService
-     */
-    public void schemaChanged(String serviceName, String version) {
-        IdServices idServices = IdServicesFactory.getDataStoreServices();
-        if (debug.messageEnabled()) {
-            debug.message("IdRepoServiceListener: Schema changed called");
-        }
-        idServices.clearIdRepoPlugins();
-
-        // Clean up cached schema plugin names
-        idServices.reloadIdRepoServiceSchema();
-    }
 }
