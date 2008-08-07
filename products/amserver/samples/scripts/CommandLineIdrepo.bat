8a85df7d5c47ba45f00736f323baa4ce240d1d6d
goodearth <goodearth@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2008-08-07 17:46:25 +0000

    Issue number: 3088 Obtained from: Submitted by: Reviewed by:  crt approver Qingwen Fixed the typo in the classname 'IdrepoSample' to 'IdRepoSample' which caused ClassNotFound Exception.
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@4364 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- products/amserver/samples/scripts/CommandLineIdrepo.bat
+++ products/amserver/samples/scripts/CommandLineIdrepo.bat
@@ -22,7 +22,7 @@
 : your own identifying information:
 : "Portions Copyrighted [year] [name of copyright owner]"
 :
-: $Id: CommandLineIdrepo.bat,v 1.5 2008-07-02 18:33:10 veiming Exp $
+: $Id: CommandLineIdrepo.bat,v 1.6 2008-08-07 17:46:25 goodearth Exp $
 :
-java -cp classes;resources;lib/openssoclientsdk.jar;lib/j2ee.jar;lib/ldapjdk.jar com.sun.identity.samples.clientsdk.idrepo.IdrepoSample
+java -cp classes;resources;lib/openssoclientsdk.jar;lib/j2ee.jar;lib/ldapjdk.jar com.sun.identity.samples.clientsdk.idrepo.IdRepoSample
 
