355c343e43d7690910591c9f950d9c06281de353
kanduls <kanduls@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2008-08-12 05:21:06 +0000

    Issue number:  3236, 3242 Obtained from: Submitted by:  kanduls Reviewed by:  young Changed FAM reference to OPENSSO and added new constants.
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@4414 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- products/amserver/source/com/sun/identity/tune/constants/FAMConstants.java
+++ products/amserver/source/com/sun/identity/tune/constants/FAMConstants.java
@@ -22,7 +22,7 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * $Id: FAMConstants.java,v 1.3 2008-07-23 17:30:47 veiming Exp $
+ * $Id: FAMConstants.java,v 1.4 2008-08-12 05:21:06 kanduls Exp $
  */
 
 package com.sun.identity.tune.constants;
@@ -32,10 +32,11 @@ package com.sun.identity.tune.constants;
  * 
  */
 public interface FAMConstants extends AMTuneConstants {
-    static String FAMADM_LOCATION = "FAMADM_LOCATION";
-    static String FAMSERVER_URL = "FAMSERVER_URL";
-    static String FAMADM_USER = "FAMADM_USER";
-    static String FAM_CONFIG_DIR = "FAM_CONFIG_DIR";
+    static String OPENSSO_SERVER_BACKUP_DIR = "conf-opensso-backup";
+    static String SSOADM_LOCATION = "SSOADM_LOCATION";
+    static String OPENSSOSERVER_URL = "OPENSSOSERVER_URL";
+    static String OPENSSOADMIN_USER = "OPENSSOADMIN_USER";
+    static String OPENSSO_CONFIG_DIR = "OPENSSO_CONFIG_DIR";
     static String REALM_NAME = "REALM_NAME";
     static String SDK_CACHE_MAXSIZE = "com.iplanet.am.sdk.cache.maxSize";
     static String NOTIFICATION_THREADPOOL_SIZE = 
@@ -48,7 +49,7 @@ public interface FAMConstants extends AMTuneConstants {
     static String SESSION_PURGE_DELAY = "com.iplanet.am.session.purgedelay";
     static String INVALID_SESSION_MAX_TIME = 
             "com.iplanet.am.session.invalidsessionmaxtime";
-    static String FAMADM_SERVER = "--servername";
+    static String SERVER_NAME_OPT = "--servername";
     static String UPDATE_SERVER_SUB_CMD = "update-server-cfg";
     static String GET_SVRCFG_XML_SUB_CMD = "get-svrcfg-xml";
     static String SET_SVRCFG_XML_SUB_CMD = "set-svrcfg-xml";
@@ -58,20 +59,26 @@ public interface FAMConstants extends AMTuneConstants {
     static String UPDATE_DATASTORE_SUB_CMD = "update-datastore";
     static String LIST_DATA_STORES_SUB_CMD = "list-datastores";
     static String LIST_SERVER_CFG_SUB_CMD = "list-server-cfg";
-    static String SUN_DSAM_SCHEMA_NAME = "Sun DS with AM schema";
-    static String GENERIC_LDAPV3 = "generic ldapv3";
+    static String SHOW_REALM_SVC_SUB_CMD = "show-realm-svcs";
+    static String GET_REALM_SVC_ATTRS_SUB_CMD = "get-realm-svc-attrs";
+    static String SET_REALM_SVC_ATTRS_SUB_CMD = "set-svc-attrs";
+    static String SESSION_SERVICE = "iPlanetAMSessionService";
     static String SERVICE_NAME_OPT = "--servicename";
     static String ATTR_NAMES_OPT = "--attributenames";
     static String OUTFILE_OPT = "--outfile";
     static String XML_FILE_OPT = "--xmlfile";
     static String SCHEMA_TYPE_OPT = "--schematype";
     static String ATTR_VALUES_OPT = "--attributevalues";
+    static String REALM_OPT = "--realm";
+    static String DATAFILE_OPT = "--datafile";
     static String MIN_CONN_POOL = "minConnPool";
     static String SMS_ELEMENT = "name=\"sms\"";
     static String DEFAULT_SERVER_ELEMENT = " name=\"default\"";
     static String MAX_CONN_POOL = "maxConnPool";
     static String AUTH_SVC = "iPlanetAMAuthService";
+    static String AUTH_LDAP_SVC = "iPlanetAMAuthLDAPService";
     static String GLOBAL_SCHEMA = "global";
+    static String ORG_SCHEMA = "organization";
     static String LDAP_CONNECTION_POOL_SIZE = 
             "iplanet-am-auth-ldap-connection-pool-default-size";
     static String LDAP_SEARCH_SCOPE = "iplanet-am-auth-ldap-search-scope";
@@ -82,4 +89,6 @@ public interface FAMConstants extends AMTuneConstants {
             "sun-idrepo-ldapv3-config-connection_pool_min_size";
     static String LDAP_CONN_POOL_MAX =
             "sun-idrepo-ldapv3-config-connection_pool_max_size";
+    static String LDAP_CONFIG_CACHE_SIZE = 
+            "sun-idrepo-ldapv3-config-cache-size";
 }
