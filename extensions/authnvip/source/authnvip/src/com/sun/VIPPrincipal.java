bcbe1554943c125eebf8eca8d92e6bf2a86aa238
superpat7 <superpat7@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2008-08-19 23:10:18 +0000

    Added standard license/copyright headers
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@4520 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- extensions/authnvip/source/authnvip/src/com/sun/VIPPrincipal.java
+++ extensions/authnvip/source/authnvip/src/com/sun/VIPPrincipal.java
@@ -1,7 +1,29 @@
-/*
- * VIPPrincipal.java
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Contributors: Terry J. Gardner, Jeff Bounds
+ * Copyright (c) 2008 Sun Microsystems Inc. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: VIPPrincipal.java,v 1.2 2008-08-19 23:10:18 superpat7 Exp $
+ *
  */
 
 package com.sun;
