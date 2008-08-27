5ef14618e571bcfe5a5795eb61c316e0b2e2219c
kanduls <kanduls@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2008-08-27 17:26:40 +0000

    Issue number:  3383 Obtained from: Submitted by: kanduls Reviewed by: Dennis Changed command line argument passing mechanism.
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@4621 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- products/federation/openfm/scripts/bin/ssoadm.bat
+++ products/federation/openfm/scripts/bin/ssoadm.bat
@@ -23,19 +23,13 @@
 : your own identifying information:
 : "Portions Copyrighted [year] [name of copyright owner]"
 :
-: $Id: ssoadm.bat,v 1.4 2008-08-13 21:40:47 dillidorai Exp $
+: $Id: ssoadm.bat,v 1.5 2008-08-27 17:26:40 kanduls Exp $
 :
 
 set TOOLS_HOME="@TOOLS_HOME@"
 
 setlocal
-:WHILE
-if x%1==x goto WEND
-set PARAMS=%PARAMS% %1
-shift
-goto WHILE
-:WEND
 
-"@JAVA_HOME@/bin/java.exe" -Xms256m -Xmx512m -cp "@CONFIG_DIR@;%TOOLS_HOME%/classes;%TOOLS_HOME%/resources;%TOOLS_HOME%/lib/amadm_setup.jar;%TOOLS_HOME%/lib/OpenDS.jar;%TOOLS_HOME%/classes;%TOOLS_HOME%/lib/ldapjdk.jar;%TOOLS_HOME%/lib/mail.jar;%TOOLS_HOME%/lib/j2ee.jar;%TOOLS_HOME%/lib/webservices-api.jar;%TOOLS_HOME%/lib/webservices-rt.jar;%TOOLS_HOME%/lib/webservices-tools.jar;%TOOLS_HOME%/lib/xsdlib.jar;%TOOLS_HOME%/lib/amserver.jar;%TOOLS_HOME%/lib/opensso-sharedlib.jar;%TOOLS_HOME%/lib/opensso.jar;%TOOLS_HOME%/lib/openfedlib.jar;%TOOLS_HOME%/../xml" -D"com.iplanet.am.serverMode=false" -D"com.sun.identity.sm.notification.enabled=false" -D"bootstrap.dir=@CONFIG_DIR@" -D"definitionFiles=com.sun.identity.cli.AccessManager,com.sun.identity.federation.cli.FederationManager" -D"commandName=ssoadm" -D"amconfig=AMConfig" -D"java.util.logging.manager=com.sun.identity.log.LogManager" -D"java.util.logging.config.class=com.sun.identity.log.s1is.LogConfigReader" -D"java.version.current=java.vm.version" -D"java.version.expected=1.4+" -D"am.version.current=com.iplanet.am.version" -D"am.version.expected=8.0" com.sun.identity.cli.CommandManager %PARAMS%
+"@JAVA_HOME@/bin/java.exe" -Xms256m -Xmx512m -cp "@CONFIG_DIR@;%TOOLS_HOME%/classes;%TOOLS_HOME%/resources;%TOOLS_HOME%/lib/amadm_setup.jar;%TOOLS_HOME%/lib/OpenDS.jar;%TOOLS_HOME%/classes;%TOOLS_HOME%/lib/ldapjdk.jar;%TOOLS_HOME%/lib/mail.jar;%TOOLS_HOME%/lib/j2ee.jar;%TOOLS_HOME%/lib/webservices-api.jar;%TOOLS_HOME%/lib/webservices-rt.jar;%TOOLS_HOME%/lib/webservices-tools.jar;%TOOLS_HOME%/lib/xsdlib.jar;%TOOLS_HOME%/lib/amserver.jar;%TOOLS_HOME%/lib/opensso-sharedlib.jar;%TOOLS_HOME%/lib/opensso.jar;%TOOLS_HOME%/lib/openfedlib.jar;%TOOLS_HOME%/../xml" -D"com.iplanet.am.serverMode=false" -D"com.sun.identity.sm.notification.enabled=false" -D"bootstrap.dir=@CONFIG_DIR@" -D"definitionFiles=com.sun.identity.cli.AccessManager,com.sun.identity.federation.cli.FederationManager" -D"commandName=ssoadm" -D"amconfig=AMConfig" -D"java.util.logging.manager=com.sun.identity.log.LogManager" -D"java.util.logging.config.class=com.sun.identity.log.s1is.LogConfigReader" -D"java.version.current=java.vm.version" -D"java.version.expected=1.4+" -D"am.version.current=com.iplanet.am.version" -D"am.version.expected=8.0" com.sun.identity.cli.CommandManager %*
 endlocal
 :END
