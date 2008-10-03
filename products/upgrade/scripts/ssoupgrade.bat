9d563771d28df80c97d6caa77a50bbd99c719343
bina <bina@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2008-10-03 06:04:28 +0000

    Issue 3740 - copy right and removal unnecessary while loop
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@5013 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- products/upgrade/scripts/ssoupgrade.bat
+++ products/upgrade/scripts/ssoupgrade.bat
@@ -1,6 +1,6 @@
 @echo off
 
-   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+: DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
   
 : Copyright (c) 2008 Sun Microsystems Inc. All Rights Reserved
   
@@ -23,21 +23,16 @@
 : your own identifying information:
 : "Portions Copyrighted [year] [name of copyright owner]"
 :
-: $Id: ssoupgrade.bat,v 1.1 2008-08-20 21:11:54 bina Exp $
+: $Id: ssoupgrade.bat,v 1.2 2008-10-03 06:04:28 bina Exp $
+
 :
+setlocal
 
 set TOOLS_HOME="STAGING_DIR"
 set CONFIG_DIR="FAM_CONFIG_DIR"
 set UPGRADE_DIR="FAM_UPGRADE_DIR"
 
-setlocal
-:WHILE
-if x%1==x goto WEND
-set PARAMS=%PARAMS% %1
-shift
-goto WHILE
-:WEND
 
-"%JAVA_HOME%/bin/java.exe" -Xms64m -Xmx256m -cp  "%UPGRADE_DIR%/locale;%UPGRADE_DIR%/lib/amadm_setup.jar;%UPGRADE_DIR%/lib/upgrade.jar;%UPGRADE_DIR%/lib/legacy.jar;%CONFIG_DIR%;%TOOLS_HOME%/locale;%TOOLS_HOME%/lib/amadm_setup.jar;%TOOLS_HOME%/lib/OpenDS.jar;%TOOLS_HOME%/classes;%TOOLS_HOME%/lib/ldapjdk.jar;%TOOLS_HOME%/lib/mail.jar;%TOOLS_HOME%/lib/j2ee.jar;%TOOLS_HOME%/lib/webservices-api.jar;%TOOLS_HOME%/lib/webservices-rt.jar;%TOOLS_HOME%/lib/webservices-tools.jar;%TOOLS_HOME%/lib/xsdlib.jar;%TOOLS_HOME%/lib/amserver.jar;%TOOLS_HOME%/lib/opensso-sharedlib.jar;%TOOLS_HOME%/lib/opensso.jar;%TOOLS_HOME%/lib/openfedlib.jar" -D"com.iplanet.am.serverMode=true" -D"bootstrap.dir=%CONFIG_DIR%" -D"definitionFiles=com.sun.identity.cli.AccessManager,com.sun.identity.federation.cli.FederationManager" -D"amconfig=AMConfig" -D"java.util.logging.manager=com.sun.identity.log.LogManager" -D"java.util.logging.config.class=com.sun.identity.log.s1is.LogConfigReader" -D"java.version.current=java.vm.version" -D"java.version.expected=1.4+" -D"am.version.current=com.iplanet.am.version" -D"am.version.expected=8.0" -D"configDir=%CONFIG_DIR%" com.sun.identity.upgrade.FAMUpgrade %PARAMS%
+"%JAVA_HOME%/bin/java.exe" -Xms64m -Xmx256m -cp  "%UPGRADE_DIR%/locale;%UPGRADE_DIR%/lib/amadm_setup.jar;%UPGRADE_DIR%/lib/upgrade.jar;%UPGRADE_DIR%/lib/legacy.jar;%CONFIG_DIR%;%TOOLS_HOME%/locale;%TOOLS_HOME%/lib/amadm_setup.jar;%TOOLS_HOME%/lib/OpenDS.jar;%TOOLS_HOME%/classes;%TOOLS_HOME%/lib/ldapjdk.jar;%TOOLS_HOME%/lib/mail.jar;%TOOLS_HOME%/lib/j2ee.jar;%TOOLS_HOME%/lib/webservices-api.jar;%TOOLS_HOME%/lib/webservices-rt.jar;%TOOLS_HOME%/lib/webservices-tools.jar;%TOOLS_HOME%/lib/xsdlib.jar;%TOOLS_HOME%/lib/amserver.jar;%TOOLS_HOME%/lib/opensso-sharedlib.jar;%TOOLS_HOME%/lib/opensso.jar;%TOOLS_HOME%/lib/openfedlib.jar" -D"com.iplanet.am.serverMode=true" -D"bootstrap.dir=%CONFIG_DIR%" -D"definitionFiles=com.sun.identity.cli.AccessManager,com.sun.identity.federation.cli.FederationManager" -D"amconfig=AMConfig" -D"java.util.logging.manager=com.sun.identity.log.LogManager" -D"java.util.logging.config.class=com.sun.identity.log.s1is.LogConfigReader" -D"java.version.current=java.vm.version" -D"java.version.expected=1.4+" -D"am.version.current=com.iplanet.am.version" -D"am.version.expected=8.0" -D"configDir=%CONFIG_DIR%" com.sun.identity.upgrade.FAMUpgrade %*
 endlocal
 :END
