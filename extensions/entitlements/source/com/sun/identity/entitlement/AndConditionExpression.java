cf6abe00c647b1ae3f4b36bd1f5f351a913e6561
dillidorai <dillidorai@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2009-01-12 22:11:22 +0000

    adding And classes for condition, entitlement, subject
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@5615 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ extensions/entitlements/source/com/sun/identity/entitlement/AndConditionExpression.java
@@ -0,0 +1,62 @@
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2008 Sun Microsystems Inc. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: AndConditionExpression.java,v 1.1 2009-01-12 22:11:22 dillidorai Exp $
+ */
+package com.sun.identity.entitlement;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.security.auth.Subject;
+
+/**
+ * Class to combine entitlement conditions using logical AND during evaluation
+ */
+public class AndConditionExpression extends ConditionExpression {
+    
+   /**
+     * Constructs condition expression
+     * @param cf condition filters that would be composed into the condition 
+     * expression
+     */
+    public AndConditionExpression(List<ConditionFilter> cf) {
+    }
+    /**
+     * Checks whether the request satisfies the <code>ConditionFilter</code>
+     * @param subject Subject who is under evaluation.
+     * @param resourceName Resource name.
+     * @param environment Environment parameters.
+     * @return <code>true</code> if the request satisfies the 
+     * <code>ConditionFilter</code>, otherwise <code>false</code>
+     * @throws com.sun.identity.entitlement.EntitlementException
+     */
+    public boolean evaluate(
+            Subject subject,
+            String resourceName,
+            Map<String, Set<String>> environment)
+            throws EntitlementException {
+        return false;
+    }
+}
