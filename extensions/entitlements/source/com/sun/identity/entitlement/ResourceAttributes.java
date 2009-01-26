3811563f1ab90bb3e1c1d448857402dc14bf299c
dillidorai <dillidorai@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2009-01-26 05:33:11 +0000

    adding ResourceAttributes interface
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@5755 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ extensions/entitlements/source/com/sun/identity/entitlement/ResourceAttributes.java
@@ -0,0 +1,70 @@
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2008 Sun Microsystems Inc. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * $Id: ResourceAttributes.java,v 1.1 2009-01-26 05:33:11 dillidorai Exp $
+ */
+package com.sun.identity.entitlement;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.security.auth.Subject;
+
+/**
+ * Interface specification for entitlement <code>ResourceAttributes</code>
+ */
+public interface ResourceAttributes {
+
+    /**
+     * Sets configuration properties for this <code>ResourceAttributes</code>
+     * @param properties configuration properties for  this
+     * <code>ResourceAttributes</code>
+     * @throws com.sun.identity.entitlement.EntitlementException if any
+     * abnormal condition occured
+     */
+    public void setProperties(Map<String, List<String>> properties)
+            throws EntitlementException;
+
+    /**
+     * <code>ResourceAttributes</code>
+     * @return configuration properties for this
+     * <code>ResourceAttributes</code>
+     */
+    public Map<String, List<String>> getProperties();
+
+    /**
+     * Returns resoruce attributes aplicable to the request
+     * @param subject Subject who is under evaluation.
+     * @param resourceName Resource name.
+     * @param environment Environment parameters.
+     * @return applicable resource attributes
+     * @throws com.sun.identity.entitlement.EntitlementException
+     * if can not get condition decision
+     */
+    public Map<String, Set<String>> evaluate(
+            Subject subject,
+            String resourceName,
+            Map<String, Set<String>> environment)
+            throws EntitlementException;
+}
