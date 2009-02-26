6dc6044597b5ca6e00831da22975827227dc9529
wstrange <wstrange@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2009-02-26 18:23:04 +0000

    Updated provider to support Spring Authorization. Fixed bug: A null OpenSSO policy decision will now result in an ABSTAIN vote.
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@6033 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- extensions/spring2provider/provider/src/main/java/com/sun/identity/provider/springsecurity/HttpUtil.java
+++ extensions/spring2provider/provider/src/main/java/com/sun/identity/provider/springsecurity/HttpUtil.java
@@ -23,11 +23,12 @@
  * 
  * "Portions Copyrighted 2008 Robert Dale <robdale@gmail.com>"
  *
- * $Id: HttpUtil.java,v 1.1 2008-12-03 00:34:24 superpat7 Exp $
+ * $Id: HttpUtil.java,v 1.2 2009-02-26 18:20:57 wstrange Exp $
  *
  */
 package com.sun.identity.provider.springsecurity;
 
+import com.sun.identity.shared.debug.Debug;
 import java.util.Enumeration;
 
 import javax.servlet.ServletRequest;
@@ -35,62 +36,59 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class HttpUtil {
+    private static Debug debug = Debug.getInstance("amSpring");
 
-	private static final Logger log = LoggerFactory.getLogger(HttpUtil.class);
+    public static void printCookies(HttpServletRequest request) {
+        if (debug.messageEnabled()) {
+            Enumeration headers = request.getHeaderNames();
+            while (headers.hasMoreElements()) {
+                String header = (String) headers.nextElement();
+                debug.message("Header: " + header + " =" +request.getHeader(header));
+            }
 
-	public static void printCookies(HttpServletRequest request) {
-		if (log.isTraceEnabled()) {
-			Enumeration headers = request.getHeaderNames();
-			while (headers.hasMoreElements()) {
-				String header = (String) headers.nextElement();
-				log.trace("Header: {} = {}", header, request.getHeader(header));
-			}
+            Cookie[] cookies = request.getCookies();
+            if (cookies == null) {
+                debug.message("Cookies are null!");
+                return;
+            }
+            if (cookies.length == 0) {
+                debug.message("Cookies are empty!");
+            } else {
+                debug.message("Cookies.length: " + cookies.length);
+                for (Cookie cookie : cookies) {
+                    String comment = cookie.getComment();
+                    String domain = cookie.getDomain();
+                    Integer maxAge = cookie.getMaxAge();
+                    String name = cookie.getName();
+                    String path = cookie.getPath();
+                    Boolean secure = cookie.getSecure();
+                    String value = cookie.getValue();
+                    Integer version = cookie.getVersion();
+                    debug.message(
+                            "Cookie: name: " + name + " domain: " + domain + " path: " + path +
+                                    " value: " + value + " secure: " + secure + " maxAge: " + maxAge + " version: " + version
+                                    + " comment " + comment );
+                           
+                }
+            }
+        }
+    }
 
-			Cookie[] cookies = request.getCookies();
-			if (cookies == null) {
-				log.trace("Cookies are null!");
-				return;
-			}
-			if (cookies.length == 0) {
-				log.trace("Cookies are empty!");
-			} else {
-				log.trace("Cookies.length: {}", cookies.length);
-				for (Cookie cookie : cookies) {
-					String comment = cookie.getComment();
-					String domain = cookie.getDomain();
-					Integer maxAge = cookie.getMaxAge();
-					String name = cookie.getName();
-					String path = cookie.getPath();
-					Boolean secure = cookie.getSecure();
-					String value = cookie.getValue();
-					Integer version = cookie.getVersion();
-					log.trace(
-							"Cookie: name: {}, domain: {}, path: {}, value: {}, secure: {}, maxAge: {}, version: {}, comment {}",
-							new Object[] { name, domain, path, value, secure, maxAge, version, comment });
-				}
-			}
-		}
-	}
-
-	public static HttpServletRequest unwrapOriginalHttpServletRequest(HttpServletRequest request) {
-		if (request instanceof HttpServletRequestWrapper) {
-			log.debug("Found HttpServletRequestWrapper: unwrapping..");
-			HttpServletRequestWrapper wrapper = (HttpServletRequestWrapper) request;
-			ServletRequest servletRequest = wrapper.getRequest();
-			if (servletRequest instanceof HttpServletRequest) {
-				log.debug("Unwrapped original HttpServletRequest");
-				request = (HttpServletRequest) servletRequest;
-			} else {
-				log.debug("Unwrapped a {}", servletRequest);
-			}
-		} else {
-			log.debug("Found a {}", request);
-		}
-		return request;
-	}
-
+    public static HttpServletRequest unwrapOriginalHttpServletRequest(HttpServletRequest request) {
+        if (request instanceof HttpServletRequestWrapper) {
+            debug.message("Found HttpServletRequestWrapper: unwrapping..");
+            HttpServletRequestWrapper wrapper = (HttpServletRequestWrapper) request;
+            ServletRequest servletRequest = wrapper.getRequest();
+            if (servletRequest instanceof HttpServletRequest) {
+                debug.message("Unwrapped original HttpServletRequest");
+                request = (HttpServletRequest) servletRequest;
+            } else {
+                debug.message("Unwrapped a " + servletRequest);
+            }
+        } else {
+            debug.message("Found a " +  request);
+        }
+        return request;
+    }
 }
