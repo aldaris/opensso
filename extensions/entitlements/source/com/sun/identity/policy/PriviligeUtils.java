1f6e80dcfe295ba8bcb2f607aae4c39e03b5b65e
dillidorai <dillidorai@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2009-02-27 06:23:47 +0000

    add debug error for non supported idType
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@6045 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- extensions/entitlements/source/com/sun/identity/policy/PriviligeUtils.java
+++ extensions/entitlements/source/com/sun/identity/policy/PriviligeUtils.java
@@ -22,9 +22,8 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * $Id: PriviligeUtils.java,v 1.6 2009-02-27 06:05:42 dillidorai Exp $
+ * $Id: PriviligeUtils.java,v 1.7 2009-02-27 06:23:47 dillidorai Exp $
  */
-
 package com.sun.identity.policy;
 
 import com.iplanet.sso.SSOToken;
@@ -46,6 +45,7 @@ import com.sun.identity.idm.IdUtils;
 import com.sun.identity.policy.interfaces.Condition;
 import com.sun.identity.policy.interfaces.ResponseProvider;
 import com.sun.identity.policy.interfaces.Subject;
+import com.sun.identity.shared.debug.Debug;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.HashSet;
@@ -72,7 +72,7 @@ public class PriviligeUtils {
      * @return entitlement Privilige object
      * @throws com.sun.identity.policy.PolicyException if the mapping fails
      */
-    public static Privilige policyToPrivilige(Policy policy) 
+    public static Privilige policyToPrivilige(Policy policy)
             throws PolicyException {
 
         if (policy == null) {
@@ -84,7 +84,7 @@ public class PriviligeUtils {
         Set ruleNames = policy.getRuleNames();
         Set<Entitlement> entitlements = new HashSet<Entitlement>();
         for (Object ruleNameObj : ruleNames) {
-            String ruleName = (String)ruleNameObj;
+            String ruleName = (String) ruleNameObj;
             Rule rule = policy.getRule(ruleName);
             Entitlement entitlement = ruleToEntitlement(rule);
             entitlements.add(entitlement);
@@ -93,7 +93,7 @@ public class PriviligeUtils {
         Set subjectNames = policy.getSubjectNames();
         Set nqSubjects = new HashSet();
         for (Object subjectNameObj : subjectNames) {
-            String subjectName = (String)subjectNameObj;
+            String subjectName = (String) subjectNameObj;
             Subject subject = policy.getSubject(subjectName);
             Set subjectValues = subject.getValues();
             boolean exclusive = policy.isSubjectExclusive(subjectName);
@@ -104,12 +104,12 @@ public class PriviligeUtils {
             nqSubjects.add(nqSubject);
         }
         ESubject eSubject = nqSubjectsToESubject(nqSubjects);
-        
+
 
         Set conditionNames = policy.getConditionNames();
         Set nConditions = new HashSet();
         for (Object conditionNameObj : conditionNames) {
-            String conditionName = (String)conditionNameObj;
+            String conditionName = (String) conditionNameObj;
             Condition condition = policy.getCondition(conditionName);
             Object[] nCondition = new Object[2];
             nCondition[0] = conditionName;
@@ -117,11 +117,11 @@ public class PriviligeUtils {
             nConditions.add(nCondition);
         }
         ECondition eCondition = nConditionsToECondition(nConditions);
-        
+
         Set rpNames = policy.getResponseProviderNames();
         Set nrps = new HashSet();
         for (Object rpNameObj : nrps) {
-            String rpName = (String)rpNameObj;
+            String rpName = (String) rpNameObj;
             ResponseProvider rp = policy.getResponseProvider(rpName);
             Object[] nrp = new Object[2];
             nrp[0] = rpName;
@@ -130,7 +130,7 @@ public class PriviligeUtils {
         }
         EResourceAttributes eResourceAttributes = nrpsToEResourceAttributes(nrps);
         Set eResourceAttributesSet = null;
-        
+
         eCondition = null;
         eResourceAttributes = null;
         Privilige privilige = new Privilige(policyName, entitlements, eSubject,
@@ -142,11 +142,10 @@ public class PriviligeUtils {
             throws PolicyException {
         String serviceName = rule.getServiceTypeName();
         String resourceName = rule.getResourceName();
-        Map<String, Object> actionMap 
-                = new HashMap<String, Object>();
+        Map<String, Object> actionMap = new HashMap<String, Object>();
         Set actionNames = rule.getActionNames();
         for (Object actionNameObj : actionNames) {
-            String actionName = (String)actionNameObj;
+            String actionName = (String) actionNameObj;
             Set actionValues = rule.getActionValues(actionName);
             actionMap.put(actionName, actionValues);
         }
@@ -157,18 +156,17 @@ public class PriviligeUtils {
         Set esSet = new HashSet();
         ESubject es = null;
         for (Object nqSubjectObj : nqSubjects) {
-            Object[] nqSubject = (Object[])nqSubjectObj;
+            Object[] nqSubject = (Object[]) nqSubjectObj;
             Set orSubjects = new HashSet();
-            String subjectName = (String)nqSubject[0];
-            Subject subject = (Subject)nqSubject[1];
-            if (subject instanceof
-                    com.sun.identity.policy.plugins.AMIdentitySubject) {
+            String subjectName = (String) nqSubject[0];
+            Subject subject = (Subject) nqSubject[1];
+            if (subject instanceof com.sun.identity.policy.plugins.AMIdentitySubject) {
                 es = mapAMIdentitySubjectToESubject(nqSubject);
             }
             esSet.add(es);
         }
         if (esSet.size() == 1) {
-            es = (ESubject)esSet.iterator().next();
+            es = (ESubject) esSet.iterator().next();
         } else if (esSet.size() > 1) {
             es = new OrESubject(esSet);
         }
@@ -178,19 +176,19 @@ public class PriviligeUtils {
     static ESubject mapAMIdentitySubjectToESubject(Object[] nqSubject) {
         Set esSet = new HashSet();
         ESubject es = null;
-        String subjectName = (String)nqSubject[0];
-        Subject subject = (Subject)nqSubject[1];
+        String subjectName = (String) nqSubject[0];
+        Subject subject = (Subject) nqSubject[1];
         Set values = subject.getValues();
         if (values == null || values.isEmpty()) {
             es = new UserESubject(null, subjectName);
-            Boolean exclusive  = (Boolean)nqSubject[2];
+            Boolean exclusive = (Boolean) nqSubject[2];
             if (exclusive) {
                 es = new NotESubject(es, subjectName);
             }
             return es;
         }
         for (Object valueObj : values) {
-            String value = (String)valueObj;
+            String value = (String) valueObj;
             AMIdentity amIdentity = null;
             SSOToken ssoToken = null;
             amIdentity = null;
@@ -211,6 +209,11 @@ public class PriviligeUtils {
                 es = new GroupESubject(value, subjectName);
             } else if (IdType.ROLE.equals(idType)) {
                 es = new RoleESubject(value, subjectName);
+            } else {
+                Debug debug = Debug.getInstance("Entitlement");
+                debug.error("PriviligeUtils.MapAMIdentitySubjectToESubject(); "
+                            + " unsupported IDType="
+                            + idType);
             }
             if (es != null) {
                 esSet.add(es);
@@ -218,27 +221,26 @@ public class PriviligeUtils {
         }
         es = null;
         if (esSet.size() == 1) {
-            es = (ESubject)esSet.iterator().next();
+            es = (ESubject) esSet.iterator().next();
         } else if (esSet.size() > 1) {
             es = new OrESubject(esSet, subjectName);
         }
-        Boolean exclusive  = (Boolean)nqSubject[2];
+        Boolean exclusive = (Boolean) nqSubject[2];
         if (exclusive) {
-           es = new NotESubject(es, subjectName); 
+            es = new NotESubject(es, subjectName);
         }
         return es;
     }
-    
 
     static ECondition nConditionsToECondition(Set nConditions) {
         JSONObject jo = new JSONObject();
         try {
             Set joys = new HashSet();
             for (Object nConditionObj : nConditions) {
-                Object[] nCondition = (Object[])nConditionObj;
+                Object[] nCondition = (Object[]) nConditionObj;
                 JSONObject joy = new JSONObject();
-                String conditionName = (String)nCondition[0];
-                Condition condition = (Condition)nCondition[1];
+                String conditionName = (String) nCondition[0];
+                Condition condition = (Condition) nCondition[1];
                 String className = condition.getClass().getName();
                 jo.put("conditionName", conditionName);
                 joy.put("className", className);
@@ -254,22 +256,21 @@ public class PriviligeUtils {
         peCondition.setState(jo.toString());
         peCondition.setPConditions(nConditions);
         return peCondition;
-        */
+         */
         return null;
     }
 
     static EResourceAttributes nrpsToEResourceAttributes(Set nprs) {
         //EResourceAttributes era = nrpsToEResourceAttributes(nrps);
-     return null;
+        return null;
     }
 
-
-    static Policy priviligeToPolicy(Privilige privilige) 
+    static Policy priviligeToPolicy(Privilige privilige)
             throws PolicyException {
         Policy policy = null;
         policy = new Policy(privilige.getName());
         Set<Entitlement> entitlements = privilige.getEntitlements();
-        for(Entitlement entitlement: entitlements) {
+        for (Entitlement entitlement : entitlements) {
             Rule rule = entitlementToRule(entitlement);
             policy.addRule(rule);
         }
@@ -286,17 +287,14 @@ public class PriviligeUtils {
     }
 
     static Set<Condition> eConditionToPConditions() {
-     return null;
+        return null;
     }
 
     static Set<EResourceAttributes> ResponseProvidersToEResourceAttributes() {
-     return null;
+        return null;
     }
 
     static Set<ResponseProvider> EResourceAttributesToResponseProviders() {
-     return null;
+        return null;
     }
-
- 
-    
 }
