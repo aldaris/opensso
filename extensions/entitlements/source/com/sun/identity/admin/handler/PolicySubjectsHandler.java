f2017713ecbf21e0257df521505be5bec14caac7
farble1670 <farble1670@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2009-04-22 22:47:31 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@6482 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

---
+++ extensions/entitlements/source/com/sun/identity/admin/handler/PolicySubjectsHandler.java
@@ -0,0 +1,7 @@
+package com.sun.identity.admin.handler;
+
+import com.icesoft.faces.component.dragdrop.DropEvent;
+
+public interface PolicySubjectsHandler {
+    public void subjectDropListener(DropEvent dropEvent);
+}
