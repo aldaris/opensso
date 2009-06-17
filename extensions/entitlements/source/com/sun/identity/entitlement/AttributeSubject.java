5ac2fc26bf8902265607196f49fd85737bf36920
hengming <hengming@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2009-06-17 02:19:53 +0000

    Issue number: Obtained from: Submitted by:  Heng-Ming Hsu Reviewed by: initial implementation
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@7167 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- extensions/entitlements/source/com/sun/identity/entitlement/AttributeSubject.java
+++ extensions/entitlements/source/com/sun/identity/entitlement/AttributeSubject.java
@@ -22,18 +22,114 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * $Id: AttributeSubject.java,v 1.1 2009-06-12 22:38:13 farble1670 Exp $
+ * $Id: AttributeSubject.java,v 1.2 2009-06-17 02:19:53 hengming Exp $
  */
 package com.sun.identity.entitlement;
 
-public class AttributeSubject extends UserSubject {
-    private String value;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.security.auth.Subject;
 
-    public String getValue() {
-        return value;
+public class AttributeSubject extends EntitlementSubjectImpl {
+    /**
+     * Constructor.
+     */
+    public AttributeSubject() {
+        super();
     }
 
+    /**
+     * Constructor.
+     *
+     * @param name attribute name
+     * @param value attibute value
+     */
+    public AttributeSubject(String name, String value) {
+        super(name, value);
+    }
+
+    /**
+     * Returns attibute value.
+     *
+     * @return attibute value
+     */
+    public String getValue() {
+        return getPSubjectName();
+    }
+
+    /**
+     * Sets attibute value.
+     *
+     * @param value attibute value
+     */
     public void setValue(String value) {
-        this.value = value;
+        setPSubjectName(value);
+    }
+
+    /**
+     * Returns <code>SubjectDecision</code> of
+     * <code>EntitlementSubject</code> evaluation
+     * @param subject EntitlementSubject who is under evaluation.
+     * @param resourceName Resource name.
+     * @param environment Environment parameters.
+     * @return <code>SubjectDecision</code> of
+     * <code>EntitlementSubject</code> evaluation
+     * @throws com.sun.identity.entitlement,  EntitlementException in case
+     * of any error
+     */
+    public SubjectDecision evaluate(
+        SubjectAttributesManager mgr,
+        Subject subject,
+        String resourceName,
+        Map<String, Set<String>> environment)
+        throws EntitlementException {
+
+        boolean satified = false;
+        Set publicCreds = subject.getPublicCredentials();
+        if ((publicCreds != null) && !publicCreds.isEmpty()) {
+            Map<String, Set<String>> attributes = (Map<String, Set<String>>)
+                publicCreds.iterator().next();
+            Set<String> values = attributes.get(
+                SubjectAttributesCollector.NAMESPACE_ATTR + getID());
+            satified = (values != null) ? values.contains(getValue()) : false;
+        }
+        return new SubjectDecision(satified, Collections.EMPTY_MAP);
+    }
+
+    /**
+     * Returns search index attributes.
+     *
+     * @return search index attributes.
+     */
+    public Map<String, Set<String>> getSearchIndexAttributes() {
+        Map<String, Set<String>> map = new HashMap<String, Set<String>>(4);
+        Set<String> set = new HashSet<String>();
+        set.add(getValue());
+        map.put(SubjectAttributesCollector.NAMESPACE_ATTR + getID(), set);
+        
+        return map;
+    }
+
+    /**
+     * Returns required attribute names.
+     * 
+     * @return required attribute names.
+     */
+    public Set<String> getRequiredAttributeNames() {
+        Set<String> set = new HashSet<String>(2);
+        set.add(SubjectAttributesCollector.NAMESPACE_ATTR + getID());
+        return set;
+    }
+
+    /**
+     * Returns <code>true</code> is this subject is an identity object.
+     *
+     * @return <code>true</code> is this subject is an identity object.
+     */
+    public boolean isIdentity() {
+        return true;
     }
 }
