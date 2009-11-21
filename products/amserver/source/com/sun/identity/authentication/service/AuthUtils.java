1f4e72daac69839a03f493642f383bc33ae7fb58
222713 <222713@b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4>
2009-11-21 02:09:40 +0000

    Issue#5811: Session time out message should be shown even if the idle time exceeds 'invalidsessionmaxtime'
    git-svn-id: https://svn.java.net/svn/opensso~svn/trunk@8278 b2119f5d-607e-cfcf-c5d3-9fa3ed8061d4

--- products/amserver/source/com/sun/identity/authentication/service/AuthUtils.java
+++ products/amserver/source/com/sun/identity/authentication/service/AuthUtils.java
@@ -22,7 +22,7 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * $Id: AuthUtils.java,v 1.29 2009-11-20 04:41:56 222713 Exp $
+ * $Id: AuthUtils.java,v 1.30 2009-11-21 02:09:40 222713 Exp $
  *
  */
 
@@ -206,6 +206,10 @@ public class AuthUtils extends AuthClientUtils {
                                   + sid);
                 utilDebug.message("AuthUtil:getAuthContext:authContext is..: "
                 + authContext);
+            }
+            if(!sid.isNull() && authContext == null) {
+                utilDebug.error("Invalid Session Timed out");
+                throw new AuthException(AMAuthErrorCode.AUTH_TIMEOUT, null);
             }
             
             if (utilDebug.messageEnabled()) {
